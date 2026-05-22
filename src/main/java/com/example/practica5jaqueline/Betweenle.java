12280facf43bc5728491e8bf2a79db319d97926d
johab <johab.montoya@uabc.edu.mx>
2026-05-22 03:00:47 -0700

    Commit Inicial

---
+++ src/main/java/com/example/practica5jaqueline/Betweenle.java
@@ -0,0 +1,10 @@
+package com.example.practica5jaqueline;
+
+public class Betweenle {
+    private String palabraSecreta;
+    private String limiteInferior;
+    private String limiteSuperior;
+    private String limiteInteriorInicial;
+    private String limiteSuperiorInicial;
+
+}
