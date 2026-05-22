a49a97d6863e1fe14b7930871a169e02f948b7dd
johab <johab.montoya@uabc.edu.mx>
2026-05-22 03:53:08 -0700

    Se agregan diccionarios y constructor y atributos extras a la clase

--- src/main/java/com/example/practica5jaqueline/Betweenle.java
+++ src/main/java/com/example/practica5jaqueline/Betweenle.java
@@ -4,7 +4,25 @@ public class Betweenle {
     private String palabraSecreta;
     private String limiteInferior;
     private String limiteSuperior;
-    private String limiteInteriorInicial;
+    private String limiteInferiorInicial;
     private String limiteSuperiorInicial;
+
+    private int intentosRestantes;
+    private int intentosMaximos;
+    private int longitudPalabra;
+
+    private String idioma;
+
+    public Betweenle(int longitudPalabra, int intentos, String idioma) {
+        this.longitudPalabra = longitudPalabra;
+        this.intentosMaximos = intentos;
+        this.intentosRestantes = intentos;
+        this.idioma = idioma.toLowerCase();
+
+        this.limiteInferiorInicial = "a".repeat(longitudPalabra);
+        this.limiteSuperiorInicial = "z".repeat(longitudPalabra);
+        this.limiteInferior = limiteInferiorInicial;
+        this.limiteSuperior = limiteSuperiorInicial;
+    }
 
 }
